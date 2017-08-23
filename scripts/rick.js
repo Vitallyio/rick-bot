3b0c7f4ec5f3112b54a9e299155491e8654db6cd
Jamie Davidson <jhdavids8@gmail.com>
2017-08-22 23:38:11 -0400

    Update Schwifty GIF

--- scripts/rick.js
+++ scripts/rick.js
@@ -1,5 +1,5 @@
 module.exports = function (robot) {
   robot.respond(/.*schwifty/i, function(msg) {
-    msg.send("http://i.imgur.com/UX6rw8w.gif");
+    msg.send("http://i.imgur.com/MXvSshF.gif");
   });
 };
